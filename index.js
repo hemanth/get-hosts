5a24e17fc6f172526d68428c4a40b0e59bc0fea9
Hemanth.HM <hemanth.hm@gmail.com>
2015-07-13 22:50:59 +0530

    Return array of objs and using stream.

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 'use strict';
-var HOSTS = process.platform === 'win32'
-  ? 'C:/Windows/System32/drivers/etc/hosts'
-  : '/etc/hosts'
+var once = require('once');
+var split = require('split');
+var through = require('through');
 var readFileSync = require('fs').readFileSync;
-module.exports = function () {
-	return readFileSync(HOSTS, { encoding: 'utf8' })
-	  .split(/\r?\n/)
-		.map(function(line){
-			// R.E from feross/hostile
-			var matches = /^\s*?([^#]+?)\s+([^#]+?)$/.exec(line)
-		    if (matches && matches.length === 3) {
-		      return [matches[1],matches[2]]
-				}
-		})
-		.filter(function(h){return !!h}); // Need to avoid this.
+var createReadStream = require('fs').createReadStream;
+var HOSTS = process.platform === 'win32' ?
+	'C:/Windows/System32/drivers/etc/hosts' : '/etc/hosts';
+var massageItem = function(line){
+    // R.E from feross/hostile
+    var matches = /^\s*?([^#]+?)\s+([^#]+?)$/.exec(line)
+    if (matches && matches.length === 3) {
+        var hostMap = {};
+        hostMap[matches[2]] = matches[1]; // host:ip
+        return hostMap;
+    }
+};
+
+var massageData = function(data) {
+    return data.split(/\r?\n/)
+        .map(massageItem)
+        .filter(Boolean);
+};
+
+var massageStream = function(){
+    return through(function write(data){
+        var hostMap = massageItem(data);
+        if (hostMap) {
+            this.queue(data);
+        }
+    })
+};
+
+module.exports = function(cb) {
+    if (typeof cb !== 'function') {
+        return massageData(readFileSync(HOSTS, {
+            encoding: 'utf8'
+        }));
+    } else {
+        cb = once(cb);
+        createReadStream(HOSTS, {
+            encoding: 'utf8'
+        })
+            .pipe(split())
+            .pipe(massageStream())
+            .on('data', function(data) {
+            //Should send the data here.
+        })
+            .on('error', function(err) {
+            cb(err, false);
+        })
+    }
 };
